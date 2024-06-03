a8dfdd1b72c67b4f6b233d9269b22d85d14f2250
RODHERMA81 <arjo1981@gmail.com>
2024-06-03 17:33:46 +0200

    correccion del ejercicio

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -4,24 +4,35 @@ public class App {
 
     public static void main( String[] args ) { 
     
-    Empleado[] empleados = {
+    Empleado[] objetos = {
     	Empleado.builder()
     		.genero(Genero.MUJER)
     		.departamento(Departamento.INFORMATICA)
     		.nombre("Maria Jose")
+    		.primerApellido("Rodriguez")
+    		.salario(2500.80)
     		.build(),
     	Empleado.builder()
     		.genero(Genero.HOMBRE)
     		.departamento(Departamento.CONTABILIDAD)
     		.nombre("Carlos")
+    		.primerApellido("Garcia")
+    		.salario(3100.50)
     		.build(),
     	Empleado.builder()
     		.genero(Genero.MUJER)
     		.departamento(Departamento.RRHH)
     		.nombre("Cristina")
+    		.primerApellido("Martinez")
+    		.salario(1400)
     		.build()
+    		
     };
-    			
+    
+    
+    double sumatoriaDeLosSalarios = 0.0;
+    
+   
     				System.out.println();
     			}
 }
