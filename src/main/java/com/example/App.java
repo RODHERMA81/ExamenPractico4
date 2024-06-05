e5d650b4a0ed8a5a0b91fea9377b61cdbdde28b7
RODHERMA81 <arjo1981@gmail.com>
2024-06-05 23:39:53 +0200

    Modificacion ejercicio 7

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -3,37 +3,30 @@ package com.example;
 public class App {
 
     public static void main( String[] args ) { 
+  
+    // Ejercicio 7a
+ 
+    
+    Estudiante[] estudiantes = {
+    Estudiante.builder()
+    	.genero(Genero.MUJER)
+    	.totalAsignaturasMatriculadas(2)
+    	.build(),
+    Estudiante.builder()
+    	.genero(Genero.MUJER)
+    	.totalAsignaturasMatriculadas(1)
+    	.build(),
+    Estudiante.builder()
+    	.genero(Genero.OTRO)
+    	.totalAsignaturasMatriculadas(5)
+    	.build()
+ 
+    };
+  
+	 
+   }
+	
    
-      
-      // Ejercicio 6a
-      Object[] empleadosYEstudiantes = {
-    		  Empleado.builder()
-    		  	.departamento(Departamento.RRHH)
-    		  	.salario(1400)
-    		  	.build(),
-    		  Empleado.builder()
-    		  	.departamento(Departamento.CONTABILIDAD)
-    		  	.salario(3100.50)
-    		  	.build(),
-    		  Estudiante.builder()
-    		  	.facultad(Facultad.MEDICINA)
-    		  	.totalAsignaturasMatriculadas(2)
-    		  	.build(),
-    		  Estudiante.builder()
-    		  	.facultad(Facultad.CIENCIAS)
-    		  	.totalAsignaturasMatriculadas(1)
-    		  	.build()
-      };
-      for(Object object : empleadosYEstudiantes) {
-    	  if(object instanceof Empleado) {
-    		Empleado empleado = (Empleado) object;
-    		
-    		System.out.println("El salario del empleado:" + empleado.getSalario());
-    	  }else if (object instanceof Estudiante){
-    	Estudiante estudiante = (Estudiante) object;
-    	
-    	System.out.println("La facultad del estudiante es :" + estudiante.getFacultad());
-    			}
-      }	
-    }
-}
+    			
+    }	
+
